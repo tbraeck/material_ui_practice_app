a87ce8a778e35232bcfda16aa2354e3f7311c48e
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-05-10 13:52:05 -0600

    Search and Data.json
    created searchappbar component, created data.json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import SearchAppBar from './components/SearchAppBar';
 import TourCard from './components/TourCard';
 // import Container from '@mui/material/Container';
 import { Grid, Container } from '@mui/material';
 
+
 function App() {
   return (
     <div className="App">
-      <Container>
+      <SearchAppBar/>
+      <Container sx={{marginY: 5}}>
           <Grid container spacing={3}>
              <TourCard/>
              <TourCard/>
