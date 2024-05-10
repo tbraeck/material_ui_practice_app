9400883a532049f9147c179ea91d2cc33310f52b
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-05-10 09:47:42 -0600

    Building Cards
    COntainer, box, grid material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import TourCard from './components/TourCard';
+// import Container from '@mui/material/Container';
+import { Grid, Container } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <TourCard/>
+      <Container>
+          <Grid container spacing={3}>
+             <TourCard/>
+             <TourCard/>
+             <TourCard/>
+             <TourCard/>
+          </Grid>
+        
+      </Container>
     </div>
   );
 }
