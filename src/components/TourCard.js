fbf349e60d9f9e827404a4ab91ccc336bcda45a5
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-05-10 09:14:19 -0600

    Beginning Build
    Initial build to practice material ui

---
+++ src/components/TourCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Card from '@mui/material/Card';
+// import CardContent from '@mui/material/CardContent';
+import { Paper } from '@mui/material';
+
+const TourCard = () => {
+  return (
+    <div>
+        <Paper>Hi</Paper>
+    </div>
+  )
+}
+
+export default TourCard
