9db5482c837ef63c40843a668d0888b3e6d6c3be
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-05-10 14:17:42 -0600

    ITERATION
    Iterated over cities and got the data.json data to show on the cards

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import './App.css';
 import SearchAppBar from './components/SearchAppBar';
 import TourCard from './components/TourCard';
 // import Container from '@mui/material/Container';
-import { Grid, Container } from '@mui/material';
-
+import { Grid, Container, Typography } from '@mui/material';
+import cities from "./data.json"
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar/>
       <Container sx={{marginY: 5}}>
+        {cities.map((city) => (
+          <>
+          <Typography 
+          variant="h4"
+          component="h2"
+          marginTop={5}
+          marginBottom={3}
+          >
+            Top {city.name} Tours
+          </Typography>
           <Grid container spacing={3}>
-             <TourCard/>
-             <TourCard/>
-             <TourCard/>
-             <TourCard/>
+            {city.tours.map((tour, index) => <TourCard tour={tour} key={index}/> )}
           </Grid>
-        
+          </>
+         ))}
       </Container>
     </div>
   );
